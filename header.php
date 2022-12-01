4d8029cd9926c1b06b06932f50a83f0d8784ad55
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-01 16:47:10 -0600

    Update header.php
    added empty navbar

--- header.php
+++ header.php
@@ -5,6 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+ 
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Hotel</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href=""></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=""></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=""></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=""></a>
+      </li>
+    </ul>
+  </div>
+</nav> 
+  
   </head>
   <body>
     
